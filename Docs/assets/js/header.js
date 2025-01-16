cff4df58c940fc5a5678368b804f85aac6b6ba82
VRYZL <s.srinath1999@gmail.com>
2025-01-16 16:12:44 +0000

    Logo changes

--- Docs/assets/js/header.js
+++ Docs/assets/js/header.js
@@ -8,7 +8,6 @@ class Header extends HTMLElement {
         <!-- Header -->
             <header id="header">
                 <a href="/index.html" class="logo">Moonlit Night Studios</a>
-                <a href="/Docs/index.html" class="logo"><strong>Documentation</strong></a>
                 <ul class="icons">
                     <li><a href="https://x.com/MoonlitNight_s" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
                 </ul>
